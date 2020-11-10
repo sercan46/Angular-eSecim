b246d33321fbd31896c674296ba6e0f237eae647
sercan46 <54890232+sercan46@users.noreply.github.com>
2020-11-10 17:26:00 +0300

    esecim

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: '/home', pathMatch: 'full'},
+  {path:'home', loadChildren: () => import('./home/home.module').then(m => m.HomeModule)},
+  {path:'girisyap', loadChildren: () => import('./oykullan/oykullan.module').then(m => m.OyKullanModule)},
+  {path:'uyeol', loadChildren: () => import('./uyeol/uyeol.module').then(m => m.UyeOlModule)},
+  {path:'secim', loadChildren: () => import('./secim/secim.module').then(m => m.SecimModule)},
+  {path:'admin', loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule)},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,39 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { HttpClientModule } from '@angular/common/http';
+import {ReactiveFormsModule} from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';
+import { MatDialogModule, MatFormFieldModule, MatIconModule } from '@angular/material';
+import { DialogBodyComponent } from './dialog-body/dialog-body.component';
+import {MatButtonModule} from '@angular/material/button';
+import { MatConfirmDialogComponent } from './mat-confirm-dialog/mat-confirm-dialog.component';
+import { QuestionDialogComponent } from './question-dialog/question-dialog.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DialogBodyComponent,
+    MatConfirmDialogComponent,
+    QuestionDialogComponent,
   ],
+
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    MatFormFieldModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot(),
+    MatDialogModule,
+    MatButtonModule,
+    MatIconModule
+
+
   ],
+  entryComponents: [DialogBodyComponent,MatConfirmDialogComponent,QuestionDialogComponent],
+
   providers: [],
   bootstrap: [AppComponent]
 })
